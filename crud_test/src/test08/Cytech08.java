39fb71ea8bd0693f9d3331efffcc8a94fa4981d8
nekokuroneko <snsd.ssset.ykr.1031@gmail.com>
2024-11-03 13:32:05 +0900

    Second Commit

--- crud_test/src/test08/Cytech08.java
+++ crud_test/src/test08/Cytech08.java
@@ -4,15 +4,10 @@ public class Cytech08 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for(int i = 0; i < 1; i++) {
-			System.out.println("0");
-			for(int j = 0; j < 1; j++) {
-				System.out.println("00");
-				for(int k = 0; k < 1; k++) {
-					System.out.println("000");
-				}	
-			}
-			System.out.println(" ");
+		String[] zero = {"0", "00", "000"};
+		
+		for(String i : zero) {
+			System.out.println(i);
 		}
 	}
 }
